e387e333a122ae3c09e323d88c2011d3a6e88551
Clarit7 <55811781+Clarit7@users.noreply.github.com>
2019-12-29 02:37:53 +0900

    깃허브 사용 연습 ~

---
+++ tf/lab03/lab_03_03.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 27 04:45:01 2019
+
+@author: extra_user
+"""
+import tensorflow as tf
+
+X = [1, 2, 3]
+Y = [1, 2, 3]
+
+W = tf.Variable(5.0)
+
+hypothesis = X * W
+
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+train = optimizer.minimize(cost)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+for step in range(100):
+    print(step, sess.run(W))
+    sess.run(train)
